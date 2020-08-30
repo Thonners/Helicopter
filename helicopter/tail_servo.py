fd7d49da12dcf4b6caab641003622dae3511f248
Mathonwy Thomas <mathonwy.thomas@gmail.com>
2020-08-30 18:20:41 +0100

    Add init method to TailServo
    - Just call super for now as we don't need any special TailServo specific code just yet
    - Also fix the heli_config.json

--- helicopter/tail_servo.py
+++ helicopter/tail_servo.py
@@ -2,4 +2,7 @@ from pithonwy.actuators.servo import Servo
 
 class TailServo(Servo):
     """ Class to hold tail servo specific admin here if required """
-    pass
+
+    def __init__(self, gpio_pin:int, centre_offset:int=0, invert_up_down:bool = False):
+        # Create the servo instance to drive the physical servo
+        super().__init__(gpio_pin=gpio_pin, centre_offset=centre_offset, invert_up_down=invert_up_down)
